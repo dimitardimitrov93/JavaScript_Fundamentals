fcd81b6f461b16d464e546faffe45510a57faba9
Dimitar Dimitrov <60186759+dimitardimitrov93@users.noreply.github.com>
2020-05-21 17:18:50 +0300

    commit

---
+++ Lecture №1 - Basic Syntax, Conditional Statements and Loops/Basic Syntax, Conditional Statements and Loops - Lab/02. Excellent grade.js
@@ -0,0 +1,12 @@
+function solve(gradeInput) {
+
+    let grade = Number(gradeInput);
+
+    if (grade >= 5.5) {
+        console.log("Excellent");
+    } else {
+        console.log("Not excellent");
+    }
+}
+
+solve(5.50);
