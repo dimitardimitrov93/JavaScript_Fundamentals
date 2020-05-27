2d5ae1f0c40b25f06ff8a4131b57c7b1aa8f598f
Dimitar Dimitrov <60186759+dimitardimitrov93@users.noreply.github.com>
2020-05-27 19:55:38 +0300

    commit

---
+++ Exercise №1 - Basic Syntax, Conditional Statemetns and Loops/Exercise/11. Bitcoin Mining.js
@@ -0,0 +1,46 @@
+function goldDigger(input) {
+
+    let days = input.length;
+    let diggedGold = 0;
+    let currentMoneyInLeva = 0;
+    let dayOfTheFirstPurchasedBitcoin = 0;
+
+    let oneBitcoinPrice = 11949.16;
+    let oneGramOfGoldPrice = 67.51;
+
+    let numberOfPurchasedBitcoins = 0;
+    let totalNumOfPurchasedBitcoins = 0;
+
+    for (let i = 1; i <= days; i++) {
+        diggedGold = Number(input.shift());
+
+        if (i % 3 == 0) {
+            diggedGold -= (diggedGold * 0.3);
+        }
+
+        currentMoneyInLeva += (parseFloat(diggedGold * oneGramOfGoldPrice));
+
+        if (currentMoneyInLeva >= oneBitcoinPrice) {
+            numberOfPurchasedBitcoins = (Math.floor(currentMoneyInLeva / oneBitcoinPrice));
+            totalNumOfPurchasedBitcoins += numberOfPurchasedBitcoins;
+
+            if (dayOfTheFirstPurchasedBitcoin < 1) {
+                dayOfTheFirstPurchasedBitcoin = i;
+            }
+
+            currentMoneyInLeva -= (numberOfPurchasedBitcoins * oneBitcoinPrice); 
+        }
+    }
+
+    currentMoneyInLeva = parseFloat(currentMoneyInLeva);
+    currentMoneyInLeva = currentMoneyInLeva.toFixed(2);
+
+    console.log(`Bought bitcoins: ${totalNumOfPurchasedBitcoins}`);
+
+    if (totalNumOfPurchasedBitcoins > 0) {
+        console.log(`Day of the first purchased bitcoin: ${dayOfTheFirstPurchasedBitcoin}`);
+    }
+    console.log(`Left money: ${currentMoneyInLeva} lv.`);
+}
+
+goldDigger([ 100, 200, 300 ]);
---
+++ Exercise №1 - Basic Syntax, Conditional Statemetns and Loops/More exercises/05. Distance between Points.js
@@ -0,0 +1,8 @@
+function solve(x1, y1, x2, y2) {
+    
+    let distanceBetweenTwoPoints = Math.sqrt((Math.pow((x2 - x1), 2)) + (Math.pow((y2 - y1), 2)));
+    console.log(distanceBetweenTwoPoints);
+
+}
+
+solve(2, 4, 5, 0);
