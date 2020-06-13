6c31a87b010d29e4dc4e832903c73daea3a9b245
Dimitar Dimitrov <60186759+dimitardimitrov93@users.noreply.github.com>
2020-06-13 15:33:36 +0300

    commit

---
+++ Lecture №4 - Functions/More exercise/02. Number Modification.js
@@ -0,0 +1,33 @@
+function modifyNumber(numberInput) {
+    let number = numberInput;
+
+    let integerToString = integer => integer.toString();
+    let stringToInteger = string => Number(string);
+
+    let currentString = '';
+
+    let currentAverageSum = 0;
+
+    function appendNumber(number) {
+
+        currentString = integerToString(number);
+        let currentSum = 0;
+
+        for (let i = 0; i < currentString.length; i++) {
+            currentSum += stringToInteger(currentString[i]);
+        }
+
+        currentAverageSum = currentSum / (currentString.length);
+
+        if (currentAverageSum <= 5) {
+            currentString += '9';
+            return appendNumber(currentString);
+        } else {
+            return stringToInteger(currentString);
+        }
+    }
+
+    console.log(appendNumber(numberInput));
+}
+
+modifyNumber(5835);
