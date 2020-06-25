052afcd18fef767a1d4e30e69883d9278172d6e2
Dimitar Dimitrov <60186759+dimitardimitrov93@users.noreply.github.com>
2020-06-26 02:27:54 +0300

    commit

---
+++ Lecture №7 - Objects & Classes/Lab/04. Object to JSON.js
@@ -0,0 +1,18 @@
+function solution(nameInput, lastNameInput, hairColorInput) {
+
+    let personInfoObj = {
+        name: nameInput,
+        lastName: lastNameInput,
+        hairColor: hairColorInput
+    }
+
+    let personInfoJson = JSON.stringify(personInfoObj);
+
+    console.log(personInfoJson);
+}
+
+solution(
+    'George',
+    'Jones',
+    'Brown'
+);
