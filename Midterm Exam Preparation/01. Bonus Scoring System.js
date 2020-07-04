9f7721082a2af6a5d82a83c4fe6ef63aa23cc505
Dimitar Dimitrov <60186759+dimitardimitrov93@users.noreply.github.com>
2020-07-04 11:47:26 +0300

    commit

--- Midterm Exam Preparation/01. Bonus Scoring System.js
+++ Midterm Exam Preparation/01. Bonus Scoring System.js
@@ -6,25 +6,40 @@ function scoringSystem(arrInput) {
     let lecturesCount = Number(arr.shift());
     let additionalCourseBonus = Number(arr.shift());
 
+    let totalMaxBonus = 0;
+    let maxAttendances = 0;
+
     let studentAttendances = arr.slice().map(function (a) {
         return Number(a);
     });
 
-    let sortedAttendances = studentAttendances.sort((a, b) => b - a);
-    let maxAttendances = sortedAttendances[0];
-    let nearestMaxAttendances = sortedAttendances[1];
+    while (studentAttendances.length > 0) {
 
-    maxAttendances = ((maxAttendances / nearestMaxAttendances) * nearestMaxAttendances);
+        let currentStudentAttendances = studentAttendances.shift();
+        let currentStudentBonus = Math.ceil((currentStudentAttendances / lecturesCount) * (5 + additionalCourseBonus));
 
-    let nearestMaxBonus = ((nearestMaxAttendances / lecturesCount) * (5 + additionalCourseBonus));
-    let totalMaxBonus = Math.ceil((((maxAttendances / lecturesCount) * (5 + additionalCourseBonus)) / nearestMaxBonus) * nearestMaxBonus);
+        if (currentStudentBonus > totalMaxBonus) {
+            totalMaxBonus = currentStudentBonus;
+            maxAttendances = currentStudentAttendances;
+        } else {
+            continue;
+        }
+
+    }
 
     console.log(`Max Bonus: ${totalMaxBonus}.`);
     console.log(`The student has attended ${maxAttendances} lectures.`);
 }
 
+// scoringSystem([
+//     '5',  '25', '30',
+//     '12', '19', '24',
+//     '16', '2'
+// ]);
+
 scoringSystem([
-    '5',  '25', '30',
-    '12', '19', '24',
-    '16', '2'
-]);
+    '10', '30', '14', '8',
+    '23', '27', '28', '15',
+    '17', '25', '26', '5',
+    '30'
+  ]);
