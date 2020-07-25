3ba819e3ee2ec2be93b23c8ec47cecb0aaceec9d
Dimitar Dimitrov <60186759+dimitardimitrov93@users.noreply.github.com>
2020-07-25 15:06:00 +0300

    commit

---
+++ Lecture №10 - Regular Expressions/Lab/02. Match Phone Number.js
@@ -0,0 +1,19 @@
+function solve(arrInput) {
+
+    let arr = arrInput.slice();
+    let unvalidatedNumbers = arr.shift();
+
+    let regExpPattern = /\+359+((\s{1})|([-]))[2]\1\d{3}\1\d{4}\b/g;
+    let validNumber;
+    let validNumbersArr = [];
+    
+    while ((validNumber = regExpPattern.exec(unvalidatedNumbers)) !== null) {
+        validNumbersArr.push(validNumber[0]);
+    }
+
+    console.log(validNumbersArr.join(', '));
+}
+
+solve([
+  "+359 2 222 2222,359-2-222-2222, +359/2/222/2222, +359-2 222 2222 +359 2-222-2222, +359-2-222-222, +359-2-222-22222 +359-2-222-2222",
+]);
